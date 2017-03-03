9eb75f2bc780959af5701c7f32aec7eb0455523c
jumorzhu(朱旻) <jumorzhu@tencent.com>
2017-03-03 18:34:44 +0800

    mock server

---
+++ mockserver/index.js
@@ -0,0 +1,6 @@
+
+ /*
+* @Author: jumorzhu@tecent.com
+* @Last Modified by:   jumorzhu
+* @Created time: 2017-03-03 10:11
+*/
---
+++ mockserver/live/list.js
@@ -0,0 +1,12 @@
+
+ /*
+* @Author: jumorzhu@tecent.com
+* @Last Modified by:   jumorzhu
+* @Created time: 2017-03-03 10:56
+*/
+
+ module.exports = function () {
+     return {
+         a:Math.random() * 3
+     }
+ }
