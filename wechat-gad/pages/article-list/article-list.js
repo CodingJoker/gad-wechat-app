40e9b45aea9a431b1c6bd7b80ec3968716804768
jumorzhu(朱旻) <jumorzhu@tencent.com>
2017-03-16 17:12:56 +0800

    'article-list'

---
+++ wechat-gad/pages/article-list/article-list.js
@@ -0,0 +1,6 @@
+var app = getApp();
+Page({
+    onLoad:function (option) {
+        console.log(`From:${option.type}`);
+    }
+})
--- wechat-gad/pages/self/self.js
+++ wechat-gad/pages/self/self.js
@@ -12,5 +12,25 @@ Page({
   onLoad:function(){
     var _this = this;
     console.log(app.globalData.userInfo);
+  },
+  toTimeLine:function () {
+      wx.navigateTo({
+        url: '../timeline/timeline'
+      });
+  },
+  toCollect:function () {
+      wx.navigateTo({
+          url: '../article-list/article-list?type=collect'
+      });
+  },
+  toArticle:function () {
+      wx.navigateTo({
+          url: '../article-list/article-list?type=article'
+      });
+  },
+  toLive:function () {
+      wx.navigateTo({
+          url: '../article-list/article-list?type=live'
+      })
   }
 })
